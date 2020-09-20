35f0fd4b57aee393ef3256ffcf699573fc22b4b8
AlienFortyHell <m17710201895@163.com>
2020-09-20 09:01:03 +0800

    坐标轴修改

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,38 +17,17 @@ namespace HSTDemo
             LineSeries ls1 = new LineSeries();
             SeriesCollection = new SeriesCollection
                 {
-                    new LineSeries
-                    {
-                        Title = "2月6日",
-                        Values = new ChartValues<double>  { -4, -4, -5, -56, -7, -8, -9, -8, -6, -4, -5, -5, -2, -1, 0, 1, 3, 6, 6, 4, 52, 1, -1, -2},
-                        //Fill = Brushes.Transparent,
+                    //new LineSeries
+                    //{
+                    //    Title = "2月6日",
+                    //    Values = new ChartValues<double>  { -4, -4, -5, -56, -7, -8, -9, -8, -6, -4, -5, -5, -2, -1, 0, 1, 3, 6, 6, 4, 52, 1, -1, -2},
+                    //    //Fill = Brushes.Transparent,
 
-                    },
-                    new LineSeries
-                    {
-                        Title = "9月5日",
-                        Values = new ChartValues<double> { 23, 22, 22, 21, -21, 21, 21, 22, 23, 25, 26, 27, 29, -30, 31, 31, 31, 30, 28, 26, 25, 24, 24, 23},
-                        //Fill = Brushes.Transparent,
-
-                    },
-
-                    new LineSeries
-                    {
-                        Title = "5月18日",
-                        Values = new ChartValues<double>  { 15, -4, -5, -6, -7, -8, 34, -8, -6, -4, 124, -5, 33, -1, 0, 1, 3, 6, 6, 4, 2, 1, -1, -2},
-                        //Fill = Brushes.Transparent,
-
-                    },
-                    new LineSeries
-                    {
-                        Title = "7月17日",
-                        Values = new ChartValues<double> { 23, 89, 22, 21, 45, 21, 21, 22, 23, -25, 26, 27, 78, 3, 4, 41, 33, 30, 28, 26, 25, 24, 24, 23},
-                        //Fill = Brushes.Transparent,
-
-                    },
+                    //},
+                 
                 };
 
-            Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            //Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
             //YFormatter = value => value.ToString("C");
 
             //modifying the series collection will animate and update the chart
@@ -66,8 +45,10 @@ namespace HSTDemo
         }
 
         public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> YFormatter { get; set; }
+        //public string[] Labels { get; set; }
+        //public Func<double, string> YFormatter { get; set; }
+
+
     }
 }
 
