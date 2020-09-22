0669fc9d163a209e98fdf06011dd3c9da932a037
zhanghuanmeng <zlkj_zhm@163.com>
2020-09-22 09:28:07 +0800

    Revert "精简代码"
    This reverts commit 2d20b5e687d28bce259dd17f163fc4c94de982da.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,24 +13,42 @@ namespace HSTDemo
         public MainWindow()
         {
             InitializeComponent();
-            ComFun.SpecificEnthalpy(3.0, 40.0);
 
-            //SeriesCollection = new SeriesCollection
-            //    {
-            //        new LineSeries
-            //        {
-            //            //Title = "2月6日",
-            //            //Values = new ChartValues<double>  { -4, -4, -5, -56, -7, -8, -9, -8, -6, -4, -5, -5, -2, -1, 0, 1, 3, 6, 6, 4, 52, 1, -1, -2},
+            LineSeries ls1 = new LineSeries();
+            SeriesCollection = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "2月6日",
+                        Values = new ChartValues<double>  { -4, -4, -5, -56, -7, -8, -9, -8, -6, -4, -5, -5, -2, -1, 0, 1, 3, 6, 6, 4, 52, 1, -1, -2},
+                        //Fill = Brushes.Transparent,
 
-            //            Values = new ChartValues<double>  { 33 },
+                    },
+                    new LineSeries
+                    {
+                        Title = "9月5日",
+                        Values = new ChartValues<double> { 23, 22, 22, 21, -21, 21, 21, 22, 23, 25, 26, 27, 29, -30, 31, 31, 31, 30, 28, 26, 25, 24, 24, 23},
+                        //Fill = Brushes.Transparent,
 
-            //            //Fill = Brushes.Transparent,
+                    },
 
-            //        },
+                    new LineSeries
+                    {
+                        Title = "5月18日",
+                        Values = new ChartValues<double>  { 15, -4, -5, -6, -7, -8, 34, -8, -6, -4, 124, -5, 33, -1, 0, 1, 3, 6, 6, 4, 2, 1, -1, -2},
+                        //Fill = Brushes.Transparent,
 
-            //    };
+                    },
+                    new LineSeries
+                    {
+                        Title = "7月17日",
+                        Values = new ChartValues<double> { 23, 89, 22, 21, 45, 21, 21, 22, 23, -25, 26, 27, 78, 3, 4, 41, 33, 30, 28, 26, 25, 24, 24, 23},
+                        //Fill = Brushes.Transparent,
 
-            //Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+                    },
+                };
+
+            Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
             //YFormatter = value => value.ToString("C");
 
             //modifying the series collection will animate and update the chart
@@ -47,9 +65,9 @@ namespace HSTDemo
 
         }
 
-        //public SeriesCollection SeriesCollection { get; set; }
-        //public string[] Labels { get; set; }
-        //public Func<double, string> YFormatter { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
     }
 }
 
