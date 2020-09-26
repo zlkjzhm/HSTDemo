a521e8faa9ddb024a0eece8802bf55a121fb7113
AlienFortyHell <m17710201895@163.com>
2020-09-27 00:36:57 +0800

    OnPropertyChanged简化

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -53,11 +53,7 @@ namespace HSTDemo
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName)
         {
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-            if(handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
 
