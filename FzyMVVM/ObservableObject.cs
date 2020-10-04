7fb67d9c318e1ff09a88722ae9a72b7952d88287
AlienFortyHell <m17710201895@163.com>
2020-10-04 17:22:00 +0800

    FzyMvvM添加

---
+++ FzyMVVM/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FzyMVVM
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
